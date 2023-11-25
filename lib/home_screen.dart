071f78e3b11660c935902bff2cbd5f4e9f4c4fe3
amit <amit.singh.9312687983@gmail.com>
2023-11-25 20:08:50 +0530

    Adding Markers of school and college

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,14 +15,45 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Completer<GoogleMapController> _controller = Completer();
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.08574965962),
+    target: LatLng(28.702070, 77.313880),
     zoom: 14.4746);
+
+  List<Marker> _marker = [];
+  final List<Marker> _list= const [
+    Marker(
+      markerId: MarkerId("1"),
+      position: LatLng(28.702070, 77.313880),
+      infoWindow: InfoWindow(
+        title: "My position"
+      )
+    ),
+    Marker(
+      markerId: MarkerId("2"),
+      position: LatLng(28.707190, 77.322180),
+      infoWindow: InfoWindow(
+        title: "Hindon"
+      )
+    ),
+    Marker(
+      markerId: MarkerId("3"),
+      position: LatLng(28.7214, 77.1409),
+      infoWindow: InfoWindow(
+        title: "Vips"
+      )
+    ),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_list);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
         myLocationEnabled: true,
+        markers: Set<Marker>.of(_marker),
         compassEnabled: true,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller){
